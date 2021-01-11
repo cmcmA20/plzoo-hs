b24f947eed0cdb5852a7778f9f999b59b97b7d4d
cmcmA20 <cmcmA20@pm.me>
2021-01-11 18:55:27 +0300

    more idiomatic applicative usage

--- src/calc/Eval.hs
+++ src/calc/Eval.hs
@@ -1,7 +1,6 @@
 -- Evaluation of expressions, given as big step semantics.
 module Eval where
 
-import Control.Applicative (liftA2)
 import Control.Effect.Throw
 
 import Syntax
@@ -13,12 +12,12 @@ type Ctx = ()
 
 eval :: Has (Throw LangError) sig m => Cmd -> m Sem
 eval (Numeral n  ) = pure n
-eval (Plus    a b) = liftA2 (+) (eval a) (eval b)
-eval (Minus   a b) = liftA2 (-) (eval a) (eval b)
-eval (Times   a b) = liftA2 (*) (eval a) (eval b)
+eval (Plus    a b) = (+) <$> eval a <*> eval b
+eval (Minus   a b) = (-) <$> eval a <*> eval b
+eval (Times   a b) = (*) <$> eval a <*> eval b
 eval (Divide  a b) = do
   y <- eval b
   if y /= 0
-     then liftA2 div (eval a) (pure y)
+     then div <$> eval a <*> pure y
      else throwError $ LERuntime $ locate Nothing "Division by zero"
 eval (Negate  a  ) = negate <$> eval a
