4b19c5277a871bb932d3d54ed9c4ed35e848548b
cmcmA20 <cmcmA20@pm.me>
2021-01-14 11:17:46 +0300

    refactor Zoo

---
+++ src/Zoo/Location.hs
@@ -0,0 +1,29 @@
+-- | Location helpers
+module Zoo.Location where
+
+import           Data.Kind (Type)
+import           Data.Text (Text)
+import qualified Data.Text                       as T
+import           GHC.Generics (Generic)
+
+data Location
+  = LNowhere -- ^ No location
+  | LLocation !Integer !Integer -- ^ Delimited location (line, column)
+  deriving (Eq, Generic, Ord)
+
+showLocation :: Location -> Text
+showLocation LNowhere        = "unknown location"
+showLocation (LLocation r c) = "line " <> T.pack (show r) <> ", column " <> T.pack (show c)
+
+instance Show Location where
+  show = T.unpack . showLocation
+
+type Located :: Type -> Type
+data Located a = MkLocated
+  { content :: !a
+  , loc     :: !Location }
+  deriving (Generic, Show)
+
+locate :: Maybe Location -> a -> Located a
+locate Nothing  x = MkLocated x LNowhere
+locate (Just l) x = MkLocated x l
---
+++ src/Zoo/Parser.hs
@@ -0,0 +1,42 @@
+-- | Command line options parsing
+module Zoo.Parser where
+
+import           Control.Algebra
+import           Control.Carrier.Reader
+import           Control.Effect.Exception
+import           Control.Effect.Lift
+import           Data.Text (Text)
+import           GHC.Generics (Generic)
+import           Options.Applicative
+
+import Zoo.Core
+
+data DefaultOpts = MkDefaultOpts
+  { nonInteractive :: !Bool
+  , onlyLangInfo   :: !Bool
+  , filesToLoad    :: ![Text] }
+  deriving (Generic, Show)
+
+defaultOpts :: Parser DefaultOpts
+defaultOpts = MkDefaultOpts
+  <$> switch (short 'n'
+    <> help "Do not run the interactive toplevel")
+  <*> switch (short 'v'
+    <> help "Print language information and exit")
+  <*> many (strOption (short 'l'
+    <> help "Load file into the initial environment"))
+
+parseOpts
+  :: forall clo sig m
+  .  ( Has (Reader (LangOpts clo)) sig m
+     , Has (Lift IO) sig m )
+  => m (clo, DefaultOpts)
+parseOpts = do
+  po <- asks @(LangOpts clo) unLangOpts
+  sendIO $ execParser $ fullOptInfo po
+  where
+    fullOptInfo :: Parser a -> ParserInfo (a, DefaultOpts)
+    fullOptInfo p = info (((,) <$> p <*> defaultOpts) <**> helper)
+      (  fullDesc
+      <> progDesc "Too many levels of abstraction"
+      <> header "The Programming Languages Zoo" )
